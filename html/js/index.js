68581563b21af7a712731b6757a2d891a9ee849d
Alexij1651 <146030955+Alexij1651@users.noreply.github.com>
2026-08-07 20:08:02 +0200

    remake calls to API, looking where is the appropiates links for the info

--- html/js/index.js
+++ html/js/index.js
@@ -2,8 +2,10 @@ const tabla = document.getElementById("cuerpo_tabla")
 fetch('https://api.openf1.org/v1/drivers?session_key=latest')
   .then(respuesta => respuesta.json())
   .then (datos => {
-tabla.array.forEach(element => {
-    
-});
+    let html = '';
+    for(const elemento of datos){html += `<tr>
+      <td>${elemento.full_name}</td>
+      </tr>`}
+      tabla.innerHTML =html;
 })
   .catch(error => console.error('Error:', error));

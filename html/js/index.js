73ad6a5e0bf09b26af1d9c6c970f41f848e46d75
Alexij1651 <146030955+Alexij1651@users.noreply.github.com>
2026-08-08 20:49:01 +0200

    Showing podium from last race on console

--- html/js/index.js
+++ html/js/index.js
@@ -1,11 +1,18 @@
-const tabla = document.getElementById("cuerpo_tabla")
-fetch('https://api.openf1.org/v1/drivers?session_key=latest')
-  .then(respuesta => respuesta.json())
-  .then (datos => {
-    let html = '';
-    for(const elemento of datos){html += `<tr>
-      <td>${elemento.full_name}</td>
-      </tr>`}
-      tabla.innerHTML =html;
-})
-  .catch(error => console.error('Error:', error));
+/*
+
+*/
+
+const tabla = document.getElementById("cuerpo_tabla");
+fetch("https://api.openf1.org/v1/session_result?session_key=latest")
+  .then((sesion) => sesion.json())
+  .then((dato1) => {
+    fetch("https://api.openf1.org/v1/drivers?session_key=latest")
+      .then((piloto) => piloto.json())
+      .then((dato2) => {
+        for (const resultado of dato1) {
+          const piloto = dato2.find(p => p.driver_number == resultado.driver_number);
+          console.log(resultado.position,piloto.full_name)
+        }
+      });
+  })
+  .catch((error) => console.error("Error:", error));

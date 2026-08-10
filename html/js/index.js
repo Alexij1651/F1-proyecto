239e7c03dca20838e1857ac20f7e001f35ccec4c
Alexij1651 <146030955+Alexij1651@users.noreply.github.com>
2026-08-10 11:30:19 +0200

    Final Grid done!

--- html/js/index.js
+++ html/js/index.js
@@ -1,18 +1,44 @@
 /*
 
 */
+setInterval(genTable, 5000);
 
-const tabla = document.getElementById("cuerpo_tabla");
-fetch("https://api.openf1.org/v1/session_result?session_key=latest")
-  .then((sesion) => sesion.json())
-  .then((dato1) => {
-    fetch("https://api.openf1.org/v1/drivers?session_key=latest")
-      .then((piloto) => piloto.json())
-      .then((dato2) => {
-        for (const resultado of dato1) {
-          const piloto = dato2.find(p => p.driver_number == resultado.driver_number);
-          console.log(resultado.position,piloto.full_name)
-        }
-      });
-  })
-  .catch((error) => console.error("Error:", error));
+function genHeader() {
+  fetch("https://api.openf1.org/v1/meetings?meeting_key=latest")
+    .then((carrera) => carrera.json())
+    .then((circuito) => {
+      document.getElementById("nombre-gp").textContent =
+        circuito[0].meeting_name;
+      document.getElementById("flag").src = circuito[0].country_flag;
+    })
+    .catch((error) => console.error("Error:", error));
+}
+
+function genTable() {
+  const tabla = document.getElementById("cuerpo_tabla");
+  fetch("https://api.openf1.org/v1/session_result?session_key=latest")
+    .then((sesion) => sesion.json())
+    .then((dato1) => {
+      fetch("https://api.openf1.org/v1/drivers?session_key=latest")
+        .then((piloto) => piloto.json())
+        .then((dato2) => {
+          let html = "";
+          for (const resultado of dato1) {
+            const piloto = dato2.find(
+              (p) => p.driver_number == resultado.driver_number,
+            );
+            html += `<tr style="border-left: 4px solid #${piloto.team_colour}">
+          <td>${resultado.position ? resultado.position : "-"}</td>
+          <td>${piloto.full_name}</td>
+          <td>${piloto.team_name}</td>
+          <td>${resultado.dnf ? "DNF" : resultado.gap_to_leader}</td>
+          </tr>`;
+          }
+          tabla.innerHTML = html;
+        });
+    })
+    .catch((error) => console.error("Error:", error));
+}
+
+genTable();
+genHeader();
